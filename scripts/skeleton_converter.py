f65180d05a89a574cfffc9afbfe09a3fc2145f86
Predrag Kostic <23319698+pkostic-dev@users.noreply.github.com>
2023-04-07 18:01:25 +0200

    Update skeleton_converter.py

--- scripts/skeleton_converter.py
+++ scripts/skeleton_converter.py
@@ -5,112 +5,29 @@ from PyNuitrack import py_nuitrack
 import numpy as np
 import logging
 
-#import cv2
-#from itertools import cycle
-
-logging.basicConfig(level=logging.DEBUG)
-
-class NuitrackSkeletonListener:
-	"""Listens for Nuitrack skeleton data.
-
-	This class initialises a Nuitrack object, setups the first device found, and
-	starts listening for skeleton data.
-	"""
-	def __init__(self):
-		super().__init__()
-
-		# Nuitrack initialisation
-		self.nuitrack = self.init_nuitrack()
-
-	def init_nuitrack(self) -> py_nuitrack.Nuitrack:
-		"""Initialises a Nuitrack object.
-		
-		This method initialises a Nuitrack object. It searches for a Nuitrack
-		compatible device and selects the first one. It checks the activation
-		status of the license. Finally it runs the Nuitrack object.
-		"""
-		logging.info("Initialising py_nuitrack.Nuitrack object.")
-		nuitrack = py_nuitrack.Nuitrack()
-		nuitrack.init()
-
-		logging.info("Getting Nuitrack device list...")
-		devices = nuitrack.get_device_list()
-		for i, dev in enumerate(devices):
-			logging.info("Device %d:\
-				\n\tname = %s,\
-				\n\tserial_number = %s",
-				i,
-				dev.get_name(),
-				dev.get_serial_number())
-			if i == 0:
-				if dev.get_activation() == "None":
-					logging.warn("No active license for this device.")
-					nuitrack.release()
-					return
-				
-				# If a license hasn't been activated with the Nuitrack Runtime
-				# package it can be activated using Python API
-				# dev.activate("ACTIVATION_KEY")
-
-				logging.info("Device %d activation status : %s",
-		 			i,
-					dev.get_activation())
-				logging.info("Setting device %d as active.", i)
-				nuitrack.set_device(dev)
-
-		logging.info("Creating nuitrack modules.")
-		nuitrack.create_modules()
-		logging.info("Running nuitrack.")
-		nuitrack.run()
-
-		return nuitrack
-
-class TFSkeletonPublisher:
-	"""Publishes tf skeleton data.
-
-	This class initialises a TransformBroadcaster object and publishes tf
-	data skeleton with it.
-	"""
-	def __init__(self):
-		super().__init__()
-
-		# Transform Broadcaster initialisation
-		logging.info("Initialising tf.TransformBroadcaster object.")
-		self.transform_broadcaster = tf.TransformBroadcaster()
-
-	def publish_data(self, skeleton_data):
-		pass
-
-	
-def nuitrack_data_to_qt_msg(data):
-	skeletons = []
-	for skelly in data.skeletons:
-		id = skelly.user_id
-		joints = skelly[1:]
-		new_skelly = dict(id = id, joints = joints)
-		skeletons.append(new_skelly)
-	data = dict(skeletons = skeletons)
-	return data
 
 class SkeletonConverter:
-	"""Converts nuitrack skeleton data into tf data.
+	"""Converts nuitrack skeleton data into tf data and publishes it.
 
-	...
+	This class creates initialises a Nuitrack object. Converts the data from it
+	into TF skeleton data. And publishes the TF skeleton data to ROS with a
+	TransformBroadcaster object.
 	"""
 
 	def __init__(self):
 		"""
 		Initialisation of class, creates Nuitrack and TransformBroadcaster
 		objects.
-		Arg:
-			self: The object pointer
 		"""
 
 		super().__init__()
 
-		
+		self.nuitrack = py_nuitrack.Nuitrack()
 
-		# TODO : update names
+		# TransformBroadcaster initialisation
+		logging.info("Initialising tf.TransformBroadcaster object.")
+		self.transform_broadcaster = tf.TransformBroadcaster()
+
 		# The ids of perced objects
 		self.id = []
 		# The joints list of skeleton. The length will be 25. Because Nuitrack
@@ -121,46 +38,119 @@ class SkeletonConverter:
 		self.translation = np.zeros([10,25,3])
 		# The rotation of every joint in space
 		self.rotation = np.zeros([10,25,9])
+
+
+	def init_nuitrack(self) -> None:
+		"""Initialises a Nuitrack object.
 		
+		This method initialises a Nuitrack object. It searches for a Nuitrack
+		compatible device and selects the first one. It checks the activation
+		status of the license. Finally it runs the Nuitrack object.
+		"""
+		
+		logging.info("Initialising py_nuitrack.Nuitrack object.")
+		self.nuitrack.init()
 
-		self.timestamp = 0
-		self.skeletons_num = 0
-		self.skeletons = []
+		logging.info("Getting Nuitrack device list...")
+		devices = self.nuitrack.get_device_list()
+		for i, dev in enumerate(devices):
+			logging.info("Device %d:\
+				\n\tname = %s,\
+				\n\tserial_number = %s",
+				i,
+				dev.get_name(),
+				dev.get_serial_number())
+			if i == 0:
+				if dev.get_activation() == "None":
+					logging.warn("No active license for device %d.", i)
+					self.nuitrack.release()
+					return
+				
+				# If a license hasn't been activated with the Nuitrack Runtime
+				# package it can be activated using Python API
+				# dev.activate("ACTIVATION_KEY")
 
-	
+				logging.info("Device %d activation status : %s",
+		 			i,
+					dev.get_activation())
+				logging.info("Setting device %d as active.", i)
+				self.nuitrack.set_device(dev)
+
+		logging.info("Creating Nuitrack modules.")
+		self.nuitrack.create_modules()
+		self.nuitrack.run()
+		logging.info("Nuitrack is running.")
+
+		# Set up a shutdown hook for releasing Nuitrack from memory.
+		rospy.on_shutdown(self.shutdown_hook)
+
 
 	def shutdown_hook(self):
 		logging.info("Shutting down nuitrack.")
 		self.nuitrack.release()
+
+
+	def convert(self, data):
+		"""Converts nuitrack skeleton data to qt skeleton data format"""
+		
+		skeletons = []
+		for skelly in data.skeletons:
+			id = skelly.user_id
+			joints = skelly[1:]
+			new_skelly = {
+				"id" : id,
+				"joints" : joints	
+			}
+			skeletons.append(new_skelly)
+		data = {
+			"skeletons" : skeletons
+		}
+		return data
+
+
+	def update(self):
+		"""Continuously update Nuitrack, get, convert, and load skeleton data."""
+
+		while not rospy.is_shutdown():
+			self.nuitrack.update()
+			data = self.convert(self.nuitrack.get_skeleton())
+			data = self.nuitrack.get_skeleton()
+			print(data)
+			# NOTE : From this point forward, black box.
+			self.load(data)
+
 	
-	def load_info(self,data):
+	def load(self,data):
 		"""
 		Function for load data heard from nuitrack	
 
 		Args:
-		self: The object pointer
 		data: The data heard from nuitrack
 		"""
+
 		# load data of every id 
 		for n in range(0,len(data.skeletons)):
 			# verifie if this id was detected before
-			if data.skeletons[n].id in self.id:
+			print(data.skeletons[n][1:])
+			
+			if data.skeletons[n].user_id in self.id:
 				# if id was detected before, we copy the joint's previous
 				# translation and rotation to the translation list and rotation
 				# list.
 				# Then Overwrite the original data of joints with the new state 
-				i = self.id.index(data.skeletons[n].id)
+				i = self.id.index(data.skeletons[n].user_id)
+
 				for j in range(0,25):
 					self.translation[i,j,:] = \
 						(-np.array(self.joints[i][j].real)/1000.0).tolist()
 					self.rotation[i,j,:] = self.joints[i][j].orientation
-				self.joints[i] = data.skeletons[n].joints
+				self.joints[i] = data.skeletons[n][1:]
 			else:
 				# if not, we will add now id and save data in the joints list,
 				# translation list and rotation list
-				self.id.append(data.skeletons[n].id)
+				self.id.append(data.skeletons[n].user_id)
 				i = len(self.id)-1
-				self.joints.append(data.skeletons[n].joints)
+				self.joints.append(data.skeletons[n][1:])
 				for j in range(0,25):
 					self.translation[i,j,:] = \
 						(-np.array(self.joints[i][j].real)/1000.0).tolist()
@@ -168,22 +158,11 @@ class SkeletonConverter:
 		self.do()
 
 
-	def msg_listener(self):
-		""" 
-		subscibe nuitrack msg
-		"""
-		rospy.on_shutdown(self.shutdown_hook)
-
-		while not rospy.is_shutdown():
-			self.nuitrack.update()
-			data = nuitrack_data_to_qt_msg(self.nuitrack.get_skeleton())
-			self.load_info(data)
-
-	
 	def do(self):
 		"""
 		Function to send all ids' TFs
 		"""
+
 		for i in self.id:
 			self.handle_tf(self.id.index(i))
 
@@ -194,6 +173,7 @@ class SkeletonConverter:
 		Args:
 			i(int): The id of detected person
 		"""
+
 		# name every joint
 		joint_name = ["Head","Neck","Torso","Waist","Left_Collar",
 			"Left_Shoulder","Left_Elbow","Left_Wrist","Left_Hand",
@@ -223,15 +203,9 @@ class SkeletonConverter:
 
 
 if __name__ == '__main__':
-	NSL = NuitrackSkeletonListener()
+	# Configure logging module
+	logging.basicConfig(level=logging.DEBUG)
 
-	logging.info("Releasing nuitrack.")
-	NSL.nuitrack.release()
-	#TF=SkeletonConverter()
-	#TF.msg_listener()
-
-# 1. init nuitrack, get device, run
-# 2. init vars
-# 3. get data from nuitrack
-# 4. translate data into tf
-# 5. publish data
+	SC = SkeletonConverter()
+	SC.init_nuitrack()
+	SC.update()
