37fa4d88496f4567e01694fac049985d3ccc3266
Predrag Kostic <23319698+pkostic-dev@users.noreply.github.com>
2023-05-17 17:10:34 +0200

    add description comment

--- scripts/python_version.py
+++ scripts/python_version.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+# You can use this script to check which version of python is currently
+# interpreting for ROS.
+
 import sys
 
 print(sys.version)
--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Script for testing.
+
 import numpy as np
 import tf
 
