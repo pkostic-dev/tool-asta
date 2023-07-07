8621bf6739b1485b412a7052d885bc3ac2fcee5f
Predrag Kostic <23319698+pkostic-dev@users.noreply.github.com>
2023-07-07 15:40:41 +0200

    Update detect_gestures.py

--- scripts/detect_gestures.py
+++ scripts/detect_gestures.py
@@ -2,19 +2,47 @@
 
 import rospy
 from tf import TransformListener
+from std_msgs.msg import String
 
 
 class GestureDetector():
     
     def __init__(self) -> None:
         super().__init__()
-        rospy.init_node("gestures", anonymous=True)
+        rospy.init_node("gesture_detector", anonymous=True)
 
+        self.root_frame = "map" # NOTE : root frame, might have to change it
+        update_frequency = 10.0  # NOTE : in Hz
+        self.rospy_rate = rospy.Rate(update_frequency)
+        self.gesture_publisher = rospy.Publisher(
+            'gesture', String, queue_size=1
+            )
         self.transform_listener = TransformListener()
         self.tfs = []
 
+        self.joints = ["head", "neck", "torso", "waist", "left_collar",
+                       "left_shoulder", "left_elbow", "left_wrist",
+                       "left_hand", "right_collar", "right_shoulder",
+                       "right_elbow", "right_wrist", "right_hand",
+                       "left_hip", "left_knee", "left_ankle",
+                       "right_hip", "right_knee", "right_ankle"]
+
+    def lookup_joints(self, joints, ids=[0]) -> list:
+        result = []
+        for id in ids:
+            for joint in joints:
+                joint_name = joint + "_" + str(id)
+                (tslt, rttn) = self.transform_listener.lookupTransform(
+                    joint_name, self.root_frame, rospy.Time(0))
+                result.append((tslt, rttn))
+                print("[%s] Joint %s : \n\t trans : %s\n\t rot : %s" %
+                    (self.t, joint_name, tslt, rttn))
+        return result
+
     def _update(self) -> None:
-        tfs = self.transform_listener
+        all_joints = self.lookup_joints(self.joints)
+        print(all_joints)
+        self.rospy_rate.sleep()
 
     def launch(self) -> None:
         """Continuously listen for tf data, and publish gestures."""
@@ -25,3 +53,50 @@ class GestureDetector():
         except KeyboardInterrupt:
             rospy.signal_shutdown("KeyboardInterrupt")
             raise SystemExit
+        
+    ###############
+
+    def detect_angle(self, joints):
+        transformations = self.lookup_joints(joints)
+        orientations = [t[1] for t in transformations]
+        angle = self.calculate_angle(*orientations)
+        if angle > 85 and angle < 95:
+            print("90 degrees")
+        print(angle)
+
+
+    def calculate_angle(self, pointA, vertex, pointB) -> float:
+        r1 = tf.transformations.quaternion_matrix(pointA)
+        r2 = tf.transformations.quaternion_matrix(vertex)
+        r3 = tf.transformations.quaternion_matrix(pointB)
+
+        relative_rotation = r2[:3, :3].T @ r1[:3, :3]
+        relative_rotation = self.pad_matrix(relative_rotation)
+
+        inverse_relative_rotation = r2[:3, :3].T @ r3[:3, :3]
+        inverse_relative_rotation = self.pad_matrix(inverse_relative_rotation)
+        
+        rotation_at_vertex = inverse_relative_rotation @ relative_rotation
+        rotation_at_vertex = self.pad_matrix(rotation_at_vertex)
+
+        rotation_quaternion = tf.transformations.quaternion_from_matrix(
+            rotation_at_vertex)
+        angles = tf.transformations.euler_from_quaternion(rotation_quaternion)
+        angle_at_vertex = angles[2]
+        angle_at_vertex_degrees = math.degrees(angle_at_vertex)
+
+        return angle_at_vertex_degrees
+
+
+    def pad_matrix(self, mat):
+        """
+        Pad a 3x3 matrix with an extra row and column to make it 4x4.
+        """
+        padded = np.pad(mat, ((0, 1), (0, 1)), mode='constant')
+        padded[3, 3] = 1.0
+        return padded
+
+
+if __name__ == "__main__":
+    GD = GestureDetector()
+    GD.launch()
