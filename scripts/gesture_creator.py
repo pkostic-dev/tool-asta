6d6dd6087c91707f36e95575e9188b0dc1c0fa61
Predrag Kostic <23319698+pkostic-dev@users.noreply.github.com>
2023-07-17 16:34:48 +0200

    formatting

--- scripts/gesture_creator.py
+++ scripts/gesture_creator.py
@@ -8,10 +8,14 @@ import pickle
 from datetime import datetime
 
 import rospy
-from tf import TransformListener, LookupException, ExtrapolationException, ConnectivityException, transformations
+from tf import TransformListener, LookupException, ExtrapolationException, ConnectivityException
 
 
 def get_key(key_timeout):
+    """
+    Check for key presses during a timeout period.
+    """
+
     file_descriptor = sys.stdin.fileno()
     old = termios.tcgetattr(file_descriptor)
 
@@ -29,6 +33,9 @@ def get_key(key_timeout):
 
 
 class GestureCreator():
+    """Allows the creation and loading of joints using simple terminal 
+    interface"""
+
     def __init__(self) -> None:
         super().__init__()
 
@@ -37,12 +44,22 @@ class GestureCreator():
         update_frequency = 10.0  # NOTE : in Hz
         self.rospy_rate = rospy.Rate(update_frequency)
         self.transform_listener = TransformListener()
+        self.all_joints = [ "head", "neck", "torso", "waist", "left_collar",
+                            "left_shoulder", "left_elbow", "left_wrist",
+                            "left_hand", "right_collar", "right_shoulder",
+                            "right_elbow", "right_wrist", "right_hand",
+                            "left_hip", "left_knee", "left_ankle",
+                            "right_hip", "right_knee", "right_ankle"]
         self.joints = ["torso"]
 
         print("Usage :")
         print("  [ Save : s ]    [ Load : l ]    [ Exit : e ]  ")
     
     def _update(self) -> None:
+        """
+        Update loop. Checks for key presses.
+        """
+
         key = get_key(0.1)
         if (len(key)):
             print("Pressed key : " + key)
@@ -50,16 +67,25 @@ class GestureCreator():
         self.rospy_rate.sleep()
 
     def _keyboard_callback(self, key) -> None:
-        if key == 's':
+        """
+        Execute functions based on key pressed.
+        """
+
+        if key == 's' or key == 'S':
             self._save()
 
-        if key == 'l':
+        if key == 'l' or key == 'L':
             self._load()
 
-        if key == 'e':
+        if key == 'e' or key == 'E':
             self._exit()
 
     def _lookup_joints(self, joints, ids=[0]) -> dict:
+        """
+        Looks up the joints in the list and returns a dictionary with
+        each joint as key and the translation and rotation arrays as values.
+        """
+
         result = dict()
         for id in ids:
             for joint in joints:
@@ -67,9 +93,14 @@ class GestureCreator():
                 (tslt, rttn) = self.transform_listener.lookupTransform(
                     joint_name, self.root_frame, rospy.Time(0))
                 result[joint] = (tslt, rttn)
+
         return result
 
     def _save(self) -> None:
+        """
+        Save sequence triggered by pressing 's'.
+        """
+
         print("Saving...")
         joints = self._lookup_joints(self.joints)
 
@@ -87,6 +118,10 @@ class GestureCreator():
         self._save_joints(joints, file_name)
 
     def _load(self) -> None:
+        """
+        Load sequence triggered by pressing 'l'.
+        """
+
         print("Loading...")
         print("Enter the file name : ", end='')
         file_name = input()
@@ -96,22 +131,40 @@ class GestureCreator():
         print(self._load_joints(file_name))
 
     def _exit(self) -> None:
+        """
+        SystemExit triggered by pressing 'e'.
+        """
+
         print("Exiting...")
         raise SystemExit
 
     def _save_joints(self, joints, file_name) -> None:
+        """
+        Save joints to a file given a file_name. Saves to working directory.
+        """
+
         with open(file_name, 'wb') as file:
             pickle.dump(joints, file)
         file.close()
 
     def _load_joints(self, file_name = 'joints.pkl') -> dict:
+        """
+        Loads file containing joints given a file name. Loads from working
+        directory.
+        """
+
         joints = dict()
         with open(file_name, 'rb') as file:
             joints = pickle.load(file)
         file.close()
+
         return joints
 
     def launch(self) -> None:
+        """
+        Continously update. Handle exceptions.
+        """
+
         while not rospy.is_shutdown():
             try:
                 self._update()
--- scripts/gesture_detector.py
+++ scripts/gesture_detector.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import pickle
 import time
 from math import degrees, sqrt
 import pprint
@@ -12,10 +13,21 @@ from std_msgs.msg import String
 
 
 class GestureDetector():
+    """Can detect angles between 3 joints, and set up trigger around a circle.
+
+    The circle trigger is set up around a given joint and radius.
+    """
     
     def __init__(self) -> None:
         super().__init__()
         rospy.init_node("gesture_detector", anonymous=True)
+
+        # Publisher of human gestures
+        self.human_gesture_publisher = rospy.Publisher(
+            'human_gesture',
+            String,
+            queue_size=1
+        )
 
         self.root_frame = "map" # NOTE : root frame, might have to change it
         update_frequency = 10.0  # NOTE : in Hz
@@ -33,8 +45,14 @@ class GestureDetector():
                        "right_elbow", "right_wrist", "right_hand",
                        "left_hip", "left_knee", "left_ankle",
                        "right_hip", "right_knee", "right_ankle"]
+        self.treasure = self._load_joints('treasure.pkl')
 
     def _lookup_joints(self, joints, ids=[0]) -> dict:
+        """
+        Looks up the joints in the list and returns a dictionary with
+        each joint as key and the translation and rotation arrays as values.
+        """
+
         result = dict()
         for id in ids:
             for joint in joints:
@@ -42,29 +60,79 @@ class GestureDetector():
                 (tslt, rttn) = self.transform_listener.lookupTransform(
                     joint_name, self.root_frame, rospy.Time(0))
                 result[joint] = (tslt, rttn)
+        
         return result
     
-    def _detect_angle(self, joints):
+    def _get_angle(self, joints) -> float:
+        """
+        Calculates the angle of the joints in list. Returns degrees.
+        """
+
         # TODO update for dict
         transformations = self._lookup_joints(joints)
         orientations = [t[1] for t in transformations]
         angle = calculate_angle(*orientations)
-        print(angle)
+        
+        return angle
 
-    def _detect_joints(self, joints) -> dict:
+    def _get_joints(self, joints) -> None:
+        """
+        Pretty prints the joints in list.
+        """
+
         jnts = self._lookup_joints(joints)
         pprint.pprint(jnts)
 
+    def _circle_trigger(self, target_joint:dict, trigger:str, rad:int=0.5):
+        """
+        Sets up a circle trigger given a target joint around which the circle
+        is set up using the radius. The joints name is used to look up the
+        realtime joint. If the realtime joint is in target joint circle,
+        trigger is published.
+        """
+
+        target_joint_name = list(target_joint.keys())[0]
+        joint = self._lookup_joints([target_joint_name])
+
+        center_x = target_joint[target_joint_name][0][0]
+        center_z = target_joint[target_joint_name][0][2]
+        point_x = joint[target_joint_name][0][0]
+        point_z = joint[target_joint_name][0][2]
+
+        if is_point_inside_circle(center_x, center_z, rad, point_x, point_z):
+            print("In circle")
+            self.human_gesture_publisher.publish(trigger)
+
+    def _load_joints(self, file_name = 'joints.pkl') -> dict:
+        """
+        Loads joints stored in file and returns as dictionary.
+        """
+
+        joints = dict()
+        with open(file_name, 'rb') as file:
+            joints = pickle.load(file)
+        file.close()
+        
+        return joints
+
     def _update(self) -> None:
+        """
+        Update loop.
+        """
+
         self.time = time.asctime(time.localtime(time.time()))
+
         # all_joints = self._lookup_joints(self.joints)
         # print(all_joints)
-        #self._detect_angle(["left_wrist", "left_elbow", "left_shoulder"])
-        self._detect_joints(["torso"])
+        # self._detect_angle(["left_wrist", "left_elbow", "left_shoulder"])
+        # self._get_joints(["torso"])
+
+        self._circle_trigger(self.treasure, "treasure_found", 0.1)
+
         self.rospy_rate.sleep()
 
     def launch(self) -> None:
-        """Continuously listen for tf data, and publish gestures."""
+        """Continuously update."""
 
         try:
             while not rospy.is_shutdown():
@@ -74,18 +142,18 @@ class GestureDetector():
                     ConnectivityException,
                     ExtrapolationException) as exception:
                     pass
-                    # msg = "[{t}] Exception {exception}"
-                    # print(msg.format(t=self.time, exception=exception))
         except KeyboardInterrupt:
             rospy.signal_shutdown("KeyboardInterrupt")
             raise SystemExit
-        
-    ###############
 
 
-
+# NOTE : Helper functions.
 
 def calculate_angle(pointA, vertex, pointB) -> float:
+    """
+    Calculates angle at vertex and returns the value in degrees.
+    """
+
     r1 = transformations.quaternion_matrix(pointA)
     r2 = transformations.quaternion_matrix(vertex)
     r3 = transformations.quaternion_matrix(pointB)
@@ -112,14 +180,21 @@ def pad_matrix(matrix) -> np.ndarray:
     """
     Pad a 3x3 matrix with an extra row and column to make it 4x4.
     """
+
     padded_matrix = np.pad(matrix, ((0, 1), (0, 1)), mode='constant')
     padded_matrix[3, 3] = 1.0
-    print(type(padded_matrix))
+
     return padded_matrix
 
 
 def is_point_inside_circle(center_x, center_y, rad, point_x, point_y) -> bool:
+    """
+    Calculates the distance between the 2 points and returns true if it's
+    smaller than the radius of the circle.
+    """
+
     distance = sqrt((point_x - center_x) ** 2 + (point_y - center_y) ** 2)
+
     return distance <= rad
 
 
